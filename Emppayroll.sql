a10623c38480131c9f076e6fc979817749609329
sonal <sonalkarle01@gmail.com>
2021-03-10 12:45:34 +0530

    Retrive the data from the DB

--- Emppayroll.sql
+++ Emppayroll.sql
@@ -14,13 +14,7 @@
 --UC1:create Db employee payroll
 create database employee_Payroll;
 select * from sys.databases;
-use employee_Payroll
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> UC1_CreateDB
-=======
-=======
->>>>>>> UC3_InsertDetails
+use employee_Payroll;
 
 --UC2:Create  employee payroll table
 create Table employee_Payroll
@@ -31,13 +25,12 @@ Salary		int,
 StartDate	Date,
 primary key(ID)
 )
-<<<<<<< HEAD
->>>>>>> UC2_CreateTable
-=======
 
 --UC3:Ability to use insert option
 insert into employee_Payroll(Name, Salary, StartDate) values
 ('dhoni', 20000, '2018-01-01'),
 ('kohli', 45000, '2019-01-01'),
 ('rahane', 22000, '2020-01-01');
->>>>>>> UC3_InsertDetails
+
+--UC4:Ability to retrive data
+select * from employee_Payroll;
